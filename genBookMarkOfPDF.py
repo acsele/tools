3d53b4512b35babab00f151dd9dba8bcb7fc9596
acsele <61129781+acsele@users.noreply.github.com>
2022-03-22 19:50:21 +0800

    Update genBookMarkOfPDF.py
    Generate bookmark file name based on original file name, no longer need to specify manually

--- genBookMarkOfPDF.py
+++ genBookMarkOfPDF.py
@@ -7,17 +7,18 @@ def addPageNo(pdfsource, txtdes):
     open(txtdes, 'w').close()
     with pdfplumber.open(pdfsource) as pdf:
         txtdesObj = open(txtdes, 'a', encoding='utf-8')
-        pre = 1
-        for p in range(pre - 1, len(pdf.pages)):
+        # Page-by-page search for text that matches heading formatting
+        for p in range(0, len(pdf.pages)):  
             text = pdf.pages[p].extract_text()
             pattern = re.compile('((?:["第"][\s][1-9][\s]["章"][\s]+(?:[\u4e00-\u9fa5a-zA-Z0-9-_——]+[\s])+)'
-                                 '|(?:(?:[1-9][.])+[0-9][\s]+(?:[\u4e00-\u9fa5a-zA-Z0-9-_——]+[\s])+))')
+                                 '|(?:(?:[1-9][.])+[1-9][\s]+(?:[\u4e00-\u9fa5a-zA-Z0-9-_——]+[\s])+))')
             matchtitle = pattern.findall(text)
             if matchtitle:
                 for title in matchtitle:
                     result = title + ';' + str(pdf.pages[p].page_number)
                     print(result)
-                    for tab in range(0, result.count(".", 0, 8)):
+                    # Hierarchical relationship between print titles
+                    for tab in range(0, result.count(".", 0, 8)):  
                         txtdesObj.write('\t')
                     txtdesObj.write(result)
                     txtdesObj.write('\n')
@@ -27,5 +28,5 @@ def addPageNo(pdfsource, txtdes):
 
 if __name__ == '__main__':
     sourcepdf = sys.argv[1]
-    despdf = sys.argv[2]
-    addPageNo(sourcepdf, despdf)
+    # despdf = sys.argv[2]
+    addPageNo(sourcepdf, 'bookMark'+sourcepdf[0:-4]+'.txt')
